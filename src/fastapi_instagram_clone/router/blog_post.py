0e2c2f92a2bbe669386fc358cc993ed1b11b8463
Derek Wan <d.wan@icloud.com>
2022-10-02 13:04:33 +0800

    Introduce `BlogModel` (#13)

--- src/fastapi_instagram_clone/router/blog_post.py
+++ src/fastapi_instagram_clone/router/blog_post.py
@@ -1,11 +1,19 @@
 from beartype import beartype
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 
 router = APIRouter(prefix="/blog", tags=["blog"])
 
 
+class BlogModel(BaseModel):
+    title: str
+    content: str
+    nb_comments: int
+    published: bool | None
+
+
 @router.post("/new")
 @beartype
-def create_blog() -> None:
-    pass
+def create_blog(*, blog: BlogModel) -> dict[str, BlogModel]:
+    return {"data": blog}
