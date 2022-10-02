3c80bb7259d92161ffa9018993755192a012f63b
Derek Wan <d.wan@icloud.com>
2022-10-02 11:36:41 +0800

    Add blog/type route

--- src/fastapi_instagram_clone/main.py
+++ src/fastapi_instagram_clone/main.py
@@ -1,5 +1,8 @@
+from enum import auto
+
 from beartype import beartype
 from fastapi import FastAPI
+from utilities.enum import StrEnum
 
 
 app = FastAPI()
@@ -15,6 +18,18 @@ def index() -> dict[str, str]:
 @beartype
 def get_all_blogs() -> dict[str, str]:
     return {"message": "All blogs provided"}
+
+
+class BlogType(StrEnum):
+    short = auto()
+    story = auto()
+    howto = auto()
+
+
+@app.get("/blog/type/{type}")
+@beartype
+def get_blog_type(*, type: BlogType) -> dict[str, str]:
+    return {"message": f"Blog with {type=}"}
 
 
 @app.get("/blog/{id}")
