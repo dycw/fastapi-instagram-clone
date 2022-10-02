4bb9d23fec8ca9e5cec6caa9592e44900a0cd1eb
Derek Wan <d.wan@icloud.com>
2022-10-02 12:18:50 +0800

    Factor out a new router into `blog_get`

--- src/fastapi_instagram_clone/main.py
+++ src/fastapi_instagram_clone/main.py
@@ -1,68 +1,14 @@
-from enum import auto
-
 from beartype import beartype
 from fastapi import FastAPI
-from fastapi import Response
-from fastapi import status
-from utilities.enum import StrEnum
+
+from fastapi_instagram_clone.router import blog_get
 
 
 app = FastAPI()
+app.include_router(blog_get.router)
 
 
 @app.get("/hello")
 @beartype
 def index() -> dict[str, str]:
     return {"message": "Hello world!"}
-
-
-@app.get(
-    "/blog/all",
-    tags=["blog"],
-    summary="Retrieve all blogs",
-    description="This API call simulates fetching all blogs.",
-    response_description="The list of available blogs",
-)
-@beartype
-def get_all_blogs(
-    *, page: int = 1, page_size: int | None = None
-) -> dict[str, str]:
-    return {"message": f"All {page_size} blogs on page {page}"}
-
-
-@app.get("/blog/{id}/comments/{comment_id}", tags=["blog", "comment"])
-@beartype
-def get_comment(
-    *, id: int, comment_id: int, valid: bool = True, username: str | None = None
-) -> dict[str, str]:
-    """Simulates retrieving a comment of a blog.
-
-    - **id** mandatory path parameter
-    - **comment_id** mandatory path parameter
-    - **valid** optional query parameter
-    - **username** optional query parameter
-    """
-    return {"message": f"{id=}, {comment_id=}, {valid=}, {username=}"}
-
-
-class BlogType(StrEnum):
-    short = auto()
-    story = auto()
-    howto = auto()
-
-
-@app.get("/blog/type/{type}", tags=["blog"])
-@beartype
-def get_blog_type(*, type: BlogType) -> dict[str, str]:
-    return {"message": f"Blog with {type=}"}
-
-
-@app.get("/blog/{id}", tags=["blog"])
-@beartype
-def get_blog(*, id: int, response: Response) -> dict[str, str]:
-    if id > 5:
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return {"error": f"Blog with {id=} not found"}
-    else:
-        response.status_code = status.HTTP_200_OK
-        return {"message": f"Blog with {id=}"}
