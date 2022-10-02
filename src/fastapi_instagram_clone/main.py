88973f6576c3f0f964a3a44d223d566a6945c022
Derek Wan <d.wan@icloud.com>
2022-10-02 12:24:26 +0800

    Add `blog_post` with new router (#12)

--- src/fastapi_instagram_clone/main.py
+++ src/fastapi_instagram_clone/main.py
@@ -2,10 +2,12 @@ from beartype import beartype
 from fastapi import FastAPI
 
 from fastapi_instagram_clone.router import blog_get
+from fastapi_instagram_clone.router import blog_post
 
 
 app = FastAPI()
 app.include_router(blog_get.router)
+app.include_router(blog_post.router)
 
 
 @app.get("/hello")
