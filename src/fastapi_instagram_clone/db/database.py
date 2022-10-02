e7c08350ca8f152d61bb11c3a4f12d98351b9b47
Derek Wan <d.wan@icloud.com>
2022-10-02 14:03:50 +0800

    Add `get_db` CM, and `DbUser` model

--- src/fastapi_instagram_clone/db/database.py
+++ src/fastapi_instagram_clone/db/database.py
@@ -1,11 +1,26 @@
+from collections.abc import Iterator
+from typing import Any
+from typing import cast
+
+from beartype import beartype
 from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./fastapi-practice.db"
-engine = create_engine(
+ENGINE = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=ENGINE)
+Base = cast(Any, declarative_base())
+
+
+@beartype
+def get_db() -> Iterator[Session]:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- src/fastapi_instagram_clone/main.py
+++ src/fastapi_instagram_clone/main.py
@@ -1,6 +1,9 @@
 from beartype import beartype
 from fastapi import FastAPI
 
+from fastapi_instagram_clone.db import models
+from fastapi_instagram_clone.db.database import ENGINE
+from fastapi_instagram_clone.db.database import Base
 from fastapi_instagram_clone.router import blog_get
 from fastapi_instagram_clone.router import blog_post
 
@@ -14,3 +17,8 @@ app.include_router(blog_post.router)
 @beartype
 def index() -> dict[str, str]:
     return {"message": "Hello world!"}
+
+
+_ = models  # for the models
+with ENGINE.begin() as conn:
+    Base.metadata.create_all(conn)
