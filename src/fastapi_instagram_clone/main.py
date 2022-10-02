5b7ce289a108c5d42c0c0bcd6a2e6a8c8ad4290b
Derek Wan <d.wan@icloud.com>
2022-10-02 11:34:45 +0800

    Add blog/all

--- src/fastapi_instagram_clone/main.py
+++ src/fastapi_instagram_clone/main.py
@@ -9,3 +9,15 @@ app = FastAPI()
 @beartype
 def index() -> dict[str, str]:
     return {"message": "Hello world!"}
+
+
+@app.get("/blog/all")
+@beartype
+def get_all_blogs() -> dict[str, str]:
+    return {"message": "All blogs provided"}
+
+
+@app.get("/blog/{id}")
+@beartype
+def get_blog(*, id: int) -> dict[str, str]:
+    return {"message": f"Blog with {id=}"}
