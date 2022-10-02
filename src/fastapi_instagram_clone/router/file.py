deffe22639da67de2ce86c28645ff519556aa235
Derek Wan <d.wan@icloud.com>
2022-10-02 23:30:55 +0800

    Add `get_uploadfile`

--- src/fastapi_instagram_clone/router/file.py
+++ src/fastapi_instagram_clone/router/file.py
@@ -1,8 +1,11 @@
+from pathlib import Path
+from shutil import copyfileobj
 from typing import Any
 
 from beartype import beartype
 from fastapi import APIRouter
 from fastapi import File
+from fastapi import UploadFile
 
 
 router = APIRouter(prefix="/file", tags=["file"])
@@ -14,3 +17,14 @@ def get_file(*, file: bytes = File(...)) -> dict[str, Any]:
     content = file.decode("utf-8")
     lines = content.split("\n")
     return {"lines": lines}
+
+
+@router.post("/uploadfile")
+# @beartype
+def get_uploadfile(*, uploadfile: UploadFile = File(...)) -> dict[str, str]:
+    filename = uploadfile.filename
+    path = Path("files", filename)
+    path.parent.mkdir(parents=True, exist_ok=True)
+    with open(path, mode="wb") as buffer:
+        copyfileobj(uploadfile.file, buffer)
+    return {"filename": filename, "type": uploadfile.content_type}
