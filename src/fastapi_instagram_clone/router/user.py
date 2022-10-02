5a3b34be6fa184811c0d86aab918d6853e4ae155
Derek Wan <d.wan@icloud.com>
2022-10-02 15:40:36 +0800

    Change `get_db` to `yield_session`, a CM (#25)

--- src/fastapi_instagram_clone/router/user.py
+++ src/fastapi_instagram_clone/router/user.py
@@ -1,15 +1,13 @@
-from contextlib import AbstractContextManager
-
 from beartype import beartype
 from fastapi import APIRouter
 from fastapi import Depends
-from sqlalchemy.orm import Session
 
-from fastapi_instagram_clone.db.database import get_db
-from fastapi_instagram_clone.db.db_user import create_user
+from fastapi_instagram_clone.db import db_user
+from fastapi_instagram_clone.db.database import yield_sess
 from fastapi_instagram_clone.db.models import DbUser
 from fastapi_instagram_clone.db.schemas import UserBase
 from fastapi_instagram_clone.db.schemas import UserDisplay
+from fastapi_instagram_clone.types import YieldSession
 
 
 router = APIRouter(prefix="/user", tags=["user"])
@@ -17,9 +15,23 @@ router = APIRouter(prefix="/user", tags=["user"])
 
 @router.post("/", response_model=UserDisplay)
 @beartype
-def create_user_endpoint(
-    *,
-    request: UserBase,
-    yield_session: AbstractContextManager[Session] = Depends(get_db),
+def create_user(
+    *, request: UserBase, yield_sess: YieldSession = Depends(yield_sess)
 ) -> DbUser:
-    return create_user(request, yield_session)
+    return db_user.create_user(request, yield_sess)
+
+
+@router.get("/", response_model=list[UserDisplay])
+@beartype
+def get_all_users(
+    *, yield_sess: YieldSession = Depends(yield_sess)
+) -> list[DbUser]:
+    return db_user.get_all_users(yield_sess)
+
+
+@router.get("/{id}", response_model=UserDisplay)
+@beartype
+def get_user(
+    *, yield_sess: YieldSession = Depends(yield_sess), id: int
+) -> DbUser:
+    return db_user.get_user(yield_sess, id)
