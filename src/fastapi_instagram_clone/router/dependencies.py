c20da988be691bb5e0038663e4b6d73ce44c384d
Derek Wan <d.wan@icloud.com>
2022-10-16 16:08:19 +0800

    Add class dependencies (#50)

--- src/fastapi_instagram_clone/router/dependencies.py
+++ src/fastapi_instagram_clone/router/dependencies.py
@@ -33,3 +33,20 @@ def create_item(
     *, headers: list[str] = Depends(convert_headers)
 ) -> dict[str, Any]:
     return {"result": "new item created", "headers": headers}
+
+
+class Account:
+    @beartype
+    def __init__(self, *, name: str, email: str) -> None:
+        super().__init__()
+        self.name = name
+        self.email = email
+
+
+@router.post("/user")
+@beartype
+def create_user(
+    *, name: str, email: str, password: str, account: Account = Depends()
+) -> dict[str, Any]:
+    _ = (name, email, password)
+    return {"name": account.name, "email": account.email}
