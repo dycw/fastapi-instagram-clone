43cb7a9ea1334d4cbe7b4d84ffae802181b607a1
Derek Wan <d.wan@icloud.com>
2022-10-16 16:13:11 +0800

    Added multi level dependencies

--- src/fastapi_instagram_clone/router/dependencies.py
+++ src/fastapi_instagram_clone/router/dependencies.py
@@ -10,20 +10,35 @@ router = APIRouter(prefix="/dependencies", tags=["dependencies"])
 
 
 @beartype
-def convert_headers(*, request: Request, separator: str = "--") -> list[str]:
+def convert_params(*, request: Request, separator: str) -> list[str]:
+    query = []
+    for key, value in request.query_params.items():
+        query.append(f"{key} {separator} {value}")
+    return query
+
+
+@beartype
+def convert_headers(
+    *,
+    request: Request,
+    separator: str = "--",
+    query: list[str] = Depends(convert_params),
+) -> dict[str, Any]:
     out_headers: list[str] = []
     for key, value in request.headers.items():
         out_headers.append(f"{key} {separator} {value}")
-    return out_headers
+    return {"headers": out_headers, "query": query}
 
 
 @router.get("/")
 @beartype
 def get_items(
     *,
-    headers: list[str] = Depends(convert_headers),
+    test: str,
+    headers: dict[str, Any] = Depends(convert_headers),
     separator: str = "--",  # noqa: U100
 ) -> dict[str, Any]:
+    _ = test
     return {"items": ["a", "b", "c"], "headers": headers}
 
 
